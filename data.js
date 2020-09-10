27e38bedbf3fd192e3c6d2756137406cf25c3e10
Aditya Pathre <pathreaig@gmail.com>
2020-09-10 01:36:54 +0100

    modified some data

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 const data =[{
-    url:"https://p16-sign-sg.tiktokcdn.com/obj/tos-alisg-p-0037/62a81fd4e146442e87af079d63dea17a_1597848443?x-expires=1599868800&amp;x-signature=Vs%2FcXuvho%2BVAOUtmP%2Fju2pzV%2BTE%3D",
-    likes:145,
-    messages:234,
-    shares:12,
-    channel:"apathre",
-    description:"I am still a song",
-    song:"I am still a song here as well",
+    "url":"https://p16-sign-sg.tiktokcdn.com/obj/tos-alisg-p-0037/62a81fd4e146442e87af079d63dea17a_1597848443?x-expires=1599868800&amp;x-signature=Vs%2FcXuvho%2BVAOUtmP%2Fju2pzV%2BTE%3D",
+    "likes":145,
+    "messages":234,
+    "shares":12,
+    "channel":"apathre",
+    "description":"I am still a song",
+    "song":"I am still a song here as well",
 
 }]
 
