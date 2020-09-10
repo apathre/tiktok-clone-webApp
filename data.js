19b4a847566ddd70be87d1b4d274cec4a9092024
Aditya Pathre <pathreaig@gmail.com>
2020-09-10 01:15:18 +0100

    updated data

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const data =[{
-    url:"https://v16m.tiktokcdn.com/f7e5fb4b733b8ee25ffa0dbd85c4e672/5f55c3ac/video/tos/useast2a/tos-useast2a-pve-0068/d8a502ec3be14ddb8a48de95dadbd720/?a=1233&br=1158&bt=579&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020090623223901018806122715188FB4&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=Mzs5dGhwZGdoczMzPDczM0ApMzs8ZjdlaTw4NzZnaTY0ZGcxZl80X2dxYWNfLS02MTZzczI1LjQvXjAuMy8vM2AxNjA6Yw%3D%3D&vl=&vr=",
+    url:"https://p16-sign-sg.tiktokcdn.com/obj/tos-alisg-p-0037/62a81fd4e146442e87af079d63dea17a_1597848443?x-expires=1599868800&amp;x-signature=Vs%2FcXuvho%2BVAOUtmP%2Fju2pzV%2BTE%3D",
     likes:145,
     messages:234,
     shares:12,
